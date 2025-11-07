14f9aa00490d8309ec0e49892ce412abe1433d03
sn82978 <shreyanakum2@gmail.com>
2025-11-07 14:26:59 -0800

    naive distance frame cmparison algorithm outputs

--- src/2D_Prototype/Noodling/convert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-NAME=""
-ffmpeg -i SampleVideos/$NAME$.mov SampleVideos/$NAME$.mp4
---
+++ src/2D_Prototype/convert.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+NAME="vial_d5"
+ffmpeg -i /home/drosophila-lab/Documents/Cameras-Calit2IRT/src/2D_Prototype/SampleVideos/$NAME.mov /home/drosophila-lab/Documents/Cameras-Calit2IRT/src/2D_Prototype/SampleVideos/$NAME.mp4
